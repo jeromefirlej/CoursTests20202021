0cd7c0f49f4fba88bc7eee882e0a3abd24330cf4
jeromefirlej <tintale@free.fr>
2020-11-09 10:13:57 +0100

    Ajout examples de cours

---
+++ ExamplesCours1/src/Math.cs
@@ -0,0 +1,10 @@
+namespace ExamplesCours
+{
+    public class Math
+    {
+        public bool estPair(int number)
+        {
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CoursTests20202021
+# TP des cours de TU en C# - EPSI 2020/2021
+
+Enoncés et corrigés des TP de TDD pour la classe EPSI B2 2020/2021
